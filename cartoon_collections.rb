c8fe4475d064f54cff1f1d5dd460af16c895072b
GIRIJA VISWANATHAN <girijavizhwa@gmail.com>
2019-12-08 03:14:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,8 +15,12 @@ def summon_captain_planet(array)
   new_arr
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.each do |ele| 
+    if ele.length > 4 
+      return true
+  end
+  return false
 end
 
 def find_the_cheese# code an argument here
